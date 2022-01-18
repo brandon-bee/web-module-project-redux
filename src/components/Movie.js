820380b213ed1a3a045e40b30b61b8433f31889a
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-01-18 14:44:22 -0800

    connected delete and add movie functions

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
+import { deleteMovie } from '../actions/movieActions';
 
 const Movie = (props) => {
   const { id } = useParams();
   const { push } = useHistory();
 
-  const { movies } = props;
+  const { movies, deleteMovie } = props;
   const movie = movies.find(movie=>movie.id===Number(id));
+
+  const handleDeleteMovie = () => {
+    deleteMovie(movie.id);
+    push('/movies');
+  }
   
   return(
     <div className="modal-page col">
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">						
-              <h4 className="modal-title">{movie.title} Details</h4>
+            <h4 className="modal-title">{movie.title} Details</h4>
           </div>
           <div className="modal-body">
             <div className="flexContainer">
@@ -40,7 +46,7 @@ const Movie = (props) => {
               
               <section>
                 <span className="m-2 btn btn-dark">Favorite</span>
-                <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                <span className="delete"><input onClick={handleDeleteMovie} type="button" className="m-2 btn btn-danger" value="Delete"/></span>
               </section>
             </div>
           </div>
@@ -56,4 +62,4 @@ const mapStateToProps = (state) => {
   });
 }
 
-export default connect(mapStateToProps)(Movie);
+export default connect(mapStateToProps, { deleteMovie })(Movie);
